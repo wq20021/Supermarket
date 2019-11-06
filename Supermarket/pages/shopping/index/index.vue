5b94f07467e22e701c0f2cb89a32d0e316eb58b9
xiaoma <13452257243@qq.com>
2019-11-06 13:21:51 +0800

    这个

---
+++ Supermarket/pages/shopping/index/index.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="page">
+	<view class="content">
+		<swiper class="content" :autoplay="true" :interval=3000 >
+			<swiper-item v-for="(img, index) in wheel" :key="index">
+				<image :src="'http://www.kupai666.com/static/goods/index_img/' + img.wheel_img" class="swiper-img"></image>
+			</swiper-item>
+		</swiper>
+		   <view class="page-body">
+			   <view class="navigation-title" v-for="(im, imgindex) in navigation" :key="imgindex">
+		                <navigator url="#" class="navigation">
+							<image :src="'http://www.kupai666.com/static/goods/index_img/' +im.navigation_img" class="navigation-img"></image>
+		                    <view class="navigation-but">{{ im.navigation_name}}</view>
+		                </navigator>
+				</view>
+		    </view>
+	</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				wheel: [],
+				navigation: [],
+			}
+		},
+		onLoad() {
+			uni.request({
+				url:'http://www.kupai666.com/index/',
+				success: (res) => {
+					this.wheel = res.data.data.wheel
+					this.navigation = res.data.data.navigation
+				}
+			})
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+@import url("index.css");
+</style>
---
+++ Supermarket/unpackage/dist/dev/mp-weixin/pages/index/user/user.js
@@ -0,0 +1,21 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/index/user/user"],{
+
+/***/ 23:
+/*!*****************************************************************************************!*\
+  !*** E:/uni_app/Supermarket/Supermarket/main.js?{"page":"pages%2Findex%2Fuser%2Fuser"} ***!
+  \*****************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 4);__webpack_require__(/*! @dcloudio/uni-stat */ 5);
+
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 2));
+var _user = _interopRequireDefault(__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module './pages/index/user/user.vue'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+createPage(_user.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ })
+
+},[[23,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../../.sourcemap/mp-weixin/pages/index/user/user.js.map
